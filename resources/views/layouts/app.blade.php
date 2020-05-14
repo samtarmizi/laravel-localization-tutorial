b1897a309c1984da37bc419e4bcfee37eb8dacef
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-05-15 01:49:18 +0800

    show current language

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,31 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        @php $locale = session()->get('locale'); @endphp
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Language <span class="caret"></span>
+                                Language 
+                                @switch($locale)
+                                    @case('fr')
+                                    <img src="{{asset('img/fr.png')}}" width="30px" height="20x"> French
+                                    @break
+                                    @case('ge')
+                                    <img src="{{asset('img/ge.png')}}" width="30px" height="20x"> German
+                                    @break
+                                    @case('es')
+                                    <img src="{{asset('img/es.png')}}" width="30px" height="20x"> Spanish
+                                    @break
+                                    @case('in')
+                                    <img src="{{asset('img/in.png')}}" width="30px" height="20x"> Hindi
+                                    @break
+                                    @case('pk')
+                                    <img src="{{asset('img/pk.png')}}" width="30px" height="20x"> Urdu
+                                    @break
+                                    @default
+                                    <img src="{{asset('img/us.png')}}" width="30px" height="20x"> English
+                                @endswitch
+                                
+                                <span class="caret"></span>
                             </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="lang/en"><img src="{{asset('img/us.png')}}" width="30px" height="20x"> English</a>
